5d65cd60e4ee1fba98afdf78d91db2fa0de7b4ea
Boaz Serem <boaserem022@gmail.com>
2026-05-06 20:45:12 +0300

    feat: split django setting  files for eas in management

---
+++ config/settings/local.py
@@ -0,0 +1,33 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+from os import getenv, path
+from dotenv import load_dotenv
+from .base import *  # noqa
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+SITE_NAME = getenv("SITE_NAME")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = getenv("DEBUG")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("ADMIN_URL")
+
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+EMAIL_FROM_EMAIL = getenv("EMAIL_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+
+
+MAX_UPLOAD_SIZE = 5 * 1024 * 1024  # 5MB
